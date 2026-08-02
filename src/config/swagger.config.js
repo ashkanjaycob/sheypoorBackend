c55bda5372eda051c45457f0b9c9b182ed5afa75
ashkanjaycob <ashkan.yaghobii@gmail.com>
2026-08-02 17:38:46 +0400

    chore: update swagger configuration metadata and formatting

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,30 +1,32 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-function SwaggerConfig (app) {
-    const swaggerDocument = swaggerJsDoc({
-        swaggerDefinition: {
-            openapi: "3.0.1",
-            info: {
-                title: "divar-backend",
-                description: "botostart nodejs course",
-                version: "1.0.0",
-            },
-            components: {
-                securitySchemes: {
-                    BearerAuth: {
-                        type: "http",
-                        scheme: "bearer",
-                        bearerFormat: "JWT",
-                    }
-                }
-            },
-            security: [{
-                BearerAuth: [],
-            }]
+function SwaggerConfig(app) {
+  const swaggerDocument = swaggerJsDoc({
+    swaggerDefinition: {
+      openapi: "3.0.1",
+      info: {
+        title: "sheypoor-backend",
+        description: "sheypoor clone backend by Ashkan Yaghoobi",
+        version: "1.0.0",
+      },
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
         },
-        apis: [process.cwd() + "/src/modules/**/*.swagger.js"]
-    });
-    const swagger = swaggerUi.setup(swaggerDocument);
-    app.use("/swagger", swaggerUi.serve, swagger);
+      },
+      security: [
+        {
+          BearerAuth: [],
+        },
+      ],
+    },
+    apis: [process.cwd() + "/src/modules/**/*.swagger.js"],
+  });
+  const swagger = swaggerUi.setup(swaggerDocument);
+  app.use("/swagger", swaggerUi.serve, swagger);
 }
 module.exports = SwaggerConfig;
